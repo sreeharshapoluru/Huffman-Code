fe17eb165b0a674f1cc81086c4472a7478c5a9e8
Sreeharsha Poluru <sreeharshapoluru@gmail.com>
2017-04-03 16:25:39 -0400

    after writing the decoder function

--- src/GenerateDecodedFiles.java
+++ src/GenerateDecodedFiles.java
@@ -28,9 +28,9 @@ public class GenerateDecodedFiles
 						{
 							if(codeArray.get(i)== 0)
 							{
-								if(tempPointer.leftChild != null)
+								if(tempPointer.leftChild == null)
 								{
-									if(i == codeArray.size()-1)
+									if(i != codeArray.size()-1)
 										tempPointer.leftChild = new Node(-1,-1);
 									else
 										tempPointer.leftChild = new Node(Integer.parseInt(code[0]),-1);
@@ -55,6 +55,7 @@ public class GenerateDecodedFiles
 						
 					}
 					bufferedInput.close();
+					this.generateDecodedText(huffmanRoot,encodedBin);
 					
 					
 						
@@ -72,6 +73,49 @@ public class GenerateDecodedFiles
 		}
 	}
 	
-	public void
+	public void generateDecodedText(Node huffmanRoot, String encodedBin)
+	{
+		try
+		{
+			
+		
+		Node tempRoot = new Node();
+		tempRoot = huffmanRoot;
+		FileInputStream fileInputStream = new FileInputStream(encodedBin);
+		FileOutputStream fileOutputStream = new FileOutputStream("/home/harsha/decoded.txt");
+		byte codeByte [] = null; 
+		fileInputStream.read(codeByte);
+		for(int i =0;i<codeByte.length;i++)
+		{
+			if(codeByte[i] == 0)
+			{
+				tempRoot = tempRoot.leftChild;
+				if(tempRoot.leftChild == null && tempRoot.rightChild == null)
+				{
+					fileOutputStream.write(tempRoot.data);
+				}
+				
+			}
+			if(codeByte[i] == 1)
+			{
+				tempRoot = tempRoot.rightChild;
+				if(tempRoot.rightChild == null && tempRoot.leftChild == null)
+			}
+		}
+		
+		
+		}
+		catch(FileNotFoundException exp)
+		{
+			System.out.println("File Not Found");
+			exp.printStackTrace();
+		}
+		catch(IOException exp)
+		{
+			System.out.println("Cannot read the file");
+			exp.printStackTrace();
+		}
+		
+	}
 
 }
